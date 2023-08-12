80a8de3a6b0ef33e36d69ff317014eb73c821231
adamaconguli <waltson2003@gmail.com>
2023-08-12 21:00:25 +0800

    search UI added

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Frame.css'
+import Searchbar from './Searchbar'
 
 export default function Frame() {
     return (
         <section className='Frame'>
-            Frame
+            <Searchbar />
         </section>
     )
 }
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import icon from '/images/search-icon.svg'
+import './Frame.css'
+
+export default function Searchbar() {
+    return (
+        <React.Fragment>
+            <input className='searchBox' type='text' placeholder='Enter Location...' />
+            <button className='searchBtn'>
+                <img src={icon} />
+            </button>
+        </React.Fragment>
+    )
+}

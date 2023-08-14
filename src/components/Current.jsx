97e95ed8ee9a439250516996e28285e7b48eddeb
adamaconguli <waltson2003@gmail.com>
2023-08-14 23:23:29 +0800

    added current weather

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import location from '/images/location.svg'
+import sunny from '/images/sunny.svg'
 
 export default function Current() {
     return (
@@ -8,6 +9,12 @@ export default function Current() {
                 <img className='location-icon' src={location} />
                 <p className='location'>Location</p>
             </div>
+            <div className='current-weather'>
+                <p className='current-tmp'>18&deg;C</p>
+                <p className='current-env'>Sunny</p>
+                <p className='current-HL'>22&deg; / 14&deg;</p>
+            </div>
+            <img className='current-img' src={sunny} />
         </React.Fragment>
     )
 }

4aefb653a60d51950e02db380f92dda88ef23850
adamaconguli <waltson2003@gmail.com>
2023-08-17 20:20:31 +0800

    forecast spelling

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './Frame.css'
 import Searchbar from './Searchbar'
 import Current from './Current'
-import HourForcast from './HourForcast'
+import HourForecast from './HourForecast'
 
 export default function Frame() {
     return (
         <main className='Frame'>
             <Searchbar />
             <Current />
-            <HourForcast />
+            <HourForecast />
         </main>
     )
 }
---
+++ src/components/HourForecast.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './HourForecast.css'
+
+export default function HourForecast() {
+    
+
+    const [hourData, setHourData] = useState([])
+
+    return(
+        <div className="HourForecast">
+            
+        </div>
+    )
+}

322ee904de4a86bf4c373d40ab08863ad2d6ce91
adamaconguli <waltson2003@gmail.com>
2023-08-18 20:51:22 +0800

    API: use netlify functions to implement hourly forcast fetch

---
+++ functions/fetch-hourly-forecast.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export async function handler(event, context) {
+    const { location, timezone } = event.queryStringParameters
+    // const { location, timezone } = { location: 106696918, timezone: 'Asia/Taipei'}
+
+    const options = {
+        method: 'GET',
+        url: `https://foreca-weather.p.rapidapi.com/forecast/hourly/${location}`,
+        params: {
+            tempunit: 'C',
+            tz: timezone,
+            periods: '12',
+            dataset: 'standard',
+        },
+        headers: {
+            'X-RapidAPI-Key': process.env.API_KEY,
+            'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com',
+        },
+    }
+
+    try {
+        const { data } = await axios.request(options)
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        }
+    } catch (error) {
+        const { status, statusText, headers, data } = error.response
+        return {
+            statusCode: status,
+            body: JSON.stringify({ status, statusText, headers, data }),
+        }
+    }
+}
--- src/components/HourForecast.jsx
+++ src/components/HourForecast.jsx
@@ -2,47 +2,42 @@ import { useEffect, useState } from 'react'
 import './HourForecast.css'
 import axios from 'axios'
 import Hour from './Hour'
-import hourForecast from '/public/HourForecast.json'
+import hourForecast from '/src/Hourforecast.json'
 
 export default function HourForecast() {
     const [hourData, setHourData] = useState([
         {
             time: 'time',
-            weather: 'weather',
-            temp: 'temp',
-            precipitation: 'precipitation',
+            symbol: 'symbol',
+            temperature: 'temperature',
+            precipProb: 'precipProb',
         },
     ])
 
     useEffect(() => {
-        // const options = {
-        //     method: 'GET',
-        //     url: 'http://localhost:8000/hourforecast',
-        //     params: {
-        //         lat: '24.99',
-        //         lon: '121.29',
-        //         timezone: 'Asia/Taipei',
-        //     },
+        // const fetchData = async () => {
+        //     const options = {
+        //         method: 'GET',
+        //         url: '/.netlify/functions/fetch-hourly-forecast',
+        //         params: {
+        //             location: 106696918,
+        //             timezone: 'Asia/Taipei',
+        //         },
+        //     }
+
+        //     const { data } = await axios.request(options)
+        //     console.log(data)
+        //     setHourData(data.forecast)
         // }
 
-        // axios
-        //     .request(options)
-        //     .then((response) => {
-        //         console.log()
-        //         setHourData(response.data.timelines.daily)
-        //     })
-        //     .catch((err) => console.log(err))
-        setHourData(hourForecast.timelines.hourly[0])
+        // fetchData().catch((error) => console.error(error))
+
+        setHourData(hourForecast)
     }, [])
 
     return (
         <div className='HourForecast'>
-            <Hour
-                time={hourData.time}
-                weather={hourData.values.weatherCode}
-                temp={hourData.values.temperature}
-                precipitation={hourData.values.precipitationProbability}
-            />
+            <Hour time={hourData[0].time} weather={hourData[0].symbol} temp={hourData[0].temperature} precipProb={hourData[0].precipProb} />
         </div>
     )
 }
